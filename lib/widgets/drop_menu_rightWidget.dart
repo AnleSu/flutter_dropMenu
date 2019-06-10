1ab2ebccfbc654e7a774116ebc6f7d355309b298
suanle <anle.su@ucarinc.com>
2019-06-10 10:41:08 +0800

    优化筛选

--- lib/widgets/drop_menu_rightWidget.dart
+++ lib/widgets/drop_menu_rightWidget.dart
@@ -2,10 +2,14 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:mynavigator/model/SearchParamModel.dart';
 
+typedef DropMenuRightCallback = void Function(SearchParamModel model, ParamItemModel item);
+
 class DropMenuRightWidget extends StatefulWidget {
   final AsyncSnapshot paramList;
+  DropMenuRightCallback clickCallBack;
   DropMenuRightWidget({
     this.paramList,
+    this.clickCallBack,
   });
 
   @override
@@ -18,8 +22,10 @@ class DropMenuRightWidget extends StatefulWidget {
 class _dropMenuRightWidgetState extends State<DropMenuRightWidget> {
   @override
   Widget buildButton(
-    String title,
-    VoidCallback onPressed, {
+    SearchParamModel model,
+    ParamItemModel item,
+    // VoidCallback onPressed, 
+    {
     bgColor = Colors.white,
     Color titleColor,
     double height = 32,
@@ -28,21 +34,17 @@ class _dropMenuRightWidgetState extends State<DropMenuRightWidget> {
     bool selected = false;
     return Container(
       height: height,
-//      margin: EdgeInsets.only(left: 20, right: 20),
       child: FlatButton(
           padding: EdgeInsets.all(0),
           onPressed: () {
-            setState(() {
-              selected = !selected;
-            });
+            widget.clickCallBack(model, item);
 
-            onPressed ?? onPressed();
+            // onPressed ?? onPressed();
           },
           child: Text(
-            title,
+            item.name,
             style: TextStyle(color: selected ? Color(0xFFF12E49) : titleColor),
           )),
-//      padding: EdgeInsets.only(left: 10, right: 10),
       decoration: BoxDecoration(
           color: bgColor,
           borderRadius: BorderRadius.all(Radius.circular(5)),
@@ -61,41 +63,49 @@ class _dropMenuRightWidgetState extends State<DropMenuRightWidget> {
     );
   }
 
+  double _screenWidth;
+
   Widget buildItem(SearchParamModel model) {
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+    _screenWidth = mediaQuery.size.width;
     return Column(
+      // shrinkWrap: true,
       children: <Widget>[
         buildTitle(model.paramName),
         Container(
-          child: Wrap(
-            alignment: WrapAlignment.start,
-            runSpacing: 10,
-            spacing: 16,
-            children: List.generate(model.itemList.length, (i) {
-              ParamItemModel item = model.itemList[i];
-              if (item.name == "自定义时间") {
-                return Container(
-                  width: double.infinity,
-                  padding:
-                      EdgeInsets.only(left: 0, right: 20, top: 0, bottom: 0),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(child: buildButton(item.name, null)),
-                      FlatButton(
-                          onPressed: null,
-                          child: Text(
-                            '清空',
-                            style: TextStyle(
-                                fontSize: 14, color: Color(0xff4A90E2)),
-                          ))
-                    ],
-                  ),
-                );
-              } else {
-                return buildButton(model.itemList[i].name, null);
-              }
-            }),
-          ),
-        ),
+            child: 
+            Wrap(
+              alignment: WrapAlignment.start,
+              // runAlignment: WrapAlignment.start,
+              runSpacing: 10,
+              spacing: 16,
+              children: List.generate(model.itemList.length, (i) {
+                ParamItemModel item = model.itemList[i];
+                if (item.name == "自定义时间") {
+                  return Container(
+                    margin: EdgeInsets.only(top: 0),
+                    padding:
+                        EdgeInsets.only(left: 0, right: 20, top: 0, bottom: 0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Expanded(child: buildButton(model, item)),
+                        FlatButton(
+                            onPressed: null,
+                            child: Text(
+                              '清空',
+                              style: TextStyle(
+                                  fontSize: 14, color: Color(0xff4A90E2)),
+                            ))
+                      ],
+                    ),
+                  );
+                } else {
+                  return buildButton(model, item);
+                }
+              }),
+            ),
+            ),
       ],
     );
   }
@@ -107,15 +117,20 @@ class _dropMenuRightWidgetState extends State<DropMenuRightWidget> {
       color: Colors.white,
       child: Column(
         children: <Widget>[
-          ListView(
-            shrinkWrap: true,
-            padding: EdgeInsets.only(left: 30, top: 9, right: 30),
-            children: List.generate(
+          Flexible(
+            child:    Container(
+            margin: EdgeInsets.only(left: 30,right: 30,top: 9),
+            child: Wrap(
+              
+               children: List.generate(
                 data.list.length,
                 (i) => Container(
                       child: buildItem(data.list[i]),
                     )),
+            ),
           ),
+          ),
+       
           Container(
             height: 1,
             color: Color(0xfff0f0f0),
@@ -123,7 +138,7 @@ class _dropMenuRightWidgetState extends State<DropMenuRightWidget> {
           ),
           Container(
             width: double.infinity,
-            padding: EdgeInsets.only(left: 30, right: 30, bottom: 16),
+            padding: EdgeInsets.only(left: 30, right: 30),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
@@ -170,8 +185,8 @@ class _dropMenuRightWidgetState extends State<DropMenuRightWidget> {
                             TextStyle(color: Color(0xFFFFFFFF), fontSize: 16),
                       )),
                   decoration: BoxDecoration(
-                      color: Color(0xFFF12E49),
-                      borderRadius: BorderRadius.all(Radius.circular(6)),
+                    color: Color(0xFFF12E49),
+                    borderRadius: BorderRadius.all(Radius.circular(6)),
                   ),
                 )),
               ],
@@ -180,103 +195,5 @@ class _dropMenuRightWidgetState extends State<DropMenuRightWidget> {
         ],
       ),
     );
-
-    // TODO: implement build
-    // return Positioned(
-    //     child: Container(
-    //   color: Colors.white,
-    //   width: double.infinity,
-    //   child: Column(
-    //     crossAxisAlignment: CrossAxisAlignment.start,
-    //     mainAxisSize: MainAxisSize.min,
-    //     children: <Widget>[
-    //       this.buildTitle('优惠活动'),
-    //       Container(
-    //         padding: EdgeInsets.only(left: 20, right: 20),
-    //         child: Row(
-    //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //           children: <Widget>[
-    //             buildButton('团购券', null),
-    //             buildButton('支付优惠', null),
-    //             buildButton('代金券', null)
-    //           ],
-    //         ),
-    //       ),
-    //       this.buildTitle('筛选时间名称'),
-    //       Container(
-    //         width: double.infinity,
-    //         padding: EdgeInsets.only(left: 20, right: 20),
-    //         child: Wrap(
-    //           alignment: WrapAlignment.spaceBetween,
-    //           runSpacing: 10,
-    //           children: <Widget>[
-    //             buildButton('今天', null),
-    //             buildButton('近2天', null),
-    //             buildButton('近7天', null),
-    //             buildButton('近15天', null),
-    //           ],
-    //         ),
-    //       ),
-    //       Container(
-    //         width: double.infinity,
-    //         padding: EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
-    //         child: Row(
-    //           children: <Widget>[
-    //             Expanded(child: buildButton('自定义日期', null)),
-    //             FlatButton(onPressed: null, child: Text('清空'))
-    //           ],
-    //         ),
-    //       ),
-    //       Container(
-    //         height: 1,
-    //         color: Colors.grey,
-    //         margin: EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
-    //       ),
-    //       Container(
-    //         width: double.infinity,
-    //         padding: EdgeInsets.only(left: 20, right: 20, bottom: 10),
-    //         child: Row(
-    //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //           children: <Widget>[
-    //             Expanded(child: buildButton('重置', null, height: 40)),
-    //             Container(
-    //               width: 10,
-    //             ),
-    //             Expanded(
-    //                 child: buildButton('确定', null,
-    //                     height: 40,
-    //                     bgColor: Colors.red,
-    //                     titleColor: Colors.white)),
-    //           ],
-    //         ),
-    //       ),
-    //     ],
-    //   ),
-    // ));
   }
-}
-
-Widget ItemContainer(String title, List data) {
-  return Container(
-    child: Column(
-      children: <Widget>[
-        Text(
-          title,
-          style: TextStyle(
-            fontSize: 14,
-            color: Color(0xFF333333),
-          ),
-        ),
-        GridView(
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-          children: <Widget>[
-            Icon(Icons.ac_unit),
-            Icon(Icons.airport_shuttle),
-            Icon(Icons.all_inclusive),
-          ],
-        )
-      ],
-    ),
-  );
 }
